6bcaabe0d6722aebf2f1f51746a2f300af29bcd1
susu3131 <anny81327@gmail.com>
2022-08-14 13:45:08 +0800

    assigment text

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,16 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+
+  $( "#reply" ).click(function() {
+    $( "#text").toggle();
+    // $( "#card-box2").toggle();
+    
+    
 });
+
+
+
+
+
+
